014ba240a82539e18c865306afbbd7e8e092069c
sandeepvarma8466 <svarma852@gmail.com>
2022-08-05 18:41:22 +0530

    GET Request Method and pass name as

--- src/main/java/com/blz/restapi/controller/RestController1.java
+++ src/main/java/com/blz/restapi/controller/RestController1.java
@@ -1,7 +1,9 @@
 package com.blz.restapi.controller;
 
+import com.blz.restapi.dto.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,12 @@ public class RestController1 {
     @GetMapping("/getdata")
     public String message() {
         return "Hello";
+    }
+
+    @GetMapping("/query")
+    public String name(@RequestParam String firstName) {
+        User user = new User();
+        user.setFirstName(firstName);
+        return user.getFirstName();
     }
 }
